cf63647205e2a7a6b56b9123a6dbd08a9ef5e2db
Ayesh Niroshan <xitebdeveloper13@gmail.com>
2024-11-28 22:47:37 +0530

    Initial commit

---
+++ src/main/java/com/ayesh/jenkins_ci_sd/JenkinsCiSdApplication.java
@@ -0,0 +1,13 @@
+package com.ayesh.jenkins_ci_sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiSdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayesh/jenkins_ci_sd/JenkinsCiSdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayesh.jenkins_ci_sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
